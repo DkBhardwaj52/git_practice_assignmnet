de23bf548620c62011d9345e72ad9d943dc2b8c9
unknown <madhusudangautam003@gmail.com>
2022-10-19 20:54:51 +0530

    change in codes

--- pallindrome.js
+++ pallindrome.js
@@ -1,4 +1,14 @@
-let x = 12431;
-if(x%10==0){
-console.log("true")
+let x = 12421;
+// palendron is a no. or string which is same when it reversed
+
+// Using String method
+
+let str = x+"";
+let new_str = "";
+let n = str.length-1;
+while(n>=0){
+    new_str+=str[n];
+    n--;
 }
+if(new_str==str) console.log("pallendron no.");
+else console.log("Not pallendron");
